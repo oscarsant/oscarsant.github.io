970bba6336c286cecd411cce6f18b018bb5d9daa
oscarsant <oscar.a.santamarina@gmail.com>
2022-02-15 01:35:34 -0500

    mobile-fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+    const players = Array.from(document.querySelectorAll('.js-player')).map(p => {
+        const plr = new Plyr(p, {
+            controls: [
+                'play-large', // The large play button in the center         
+                'play', // Play/pause playback      
+                'progress', // The progress bar and scrubber for playback and buffering    
+                // 'current-time', // The current time of playback    
+                // 'duration', // The full duration of the media 
+                'mute',
+                'fullscreen'
+
+            ],
+
+            // fullscreen: { enabled: false, fallback: false, iosNative: false, container: null }
+
+        })
+    });
 
     const lightBoxCont = document.querySelectorAll('.lightbox--cont');
     lightBoxCont.forEach(el => {
@@ -13,7 +30,7 @@ $(document).ready(function() {
 
         bclick.addEventListener("click", event => {
             lightBox.className = "lightbox open";
-            
+
             // console.log(lightBox)
         });
 
@@ -29,7 +46,17 @@ $(document).ready(function() {
             }
         });
     });
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
 
 
